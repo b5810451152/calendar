be4d52b76461dfd860021a9fbaea13db61945ef9
b5810451152 <31498490+b5810451152@users.noreply.github.com>
2017-09-01 07:55:15 +0700

    Add files via upload

--- src/test/java/ku/sci/cs/calendar/MyAppTest.java
+++ src/test/java/ku/sci/cs/calendar/MyAppTest.java
@@ -4,11 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Model.Calendar;
+
 public class MyAppTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Calendar cal = new  Calendar();
+		assertEquals(cal.showEvent("January"),"You don't have events in this month");
+		cal.addEvent("July 27 Birthday", 0);
+		assertEquals(cal.showEvent("July"),"July 27 Birthday\n");
+		cal.addEvent("July 31 Nut'Birthday", 0);
+		assertEquals(cal.showEvent("July"),"July 27 Birthday\nJuly 31 Nut'Birthday\n");
+		
+		cal.addEvent("June 18 midterm exam", 0);
+		assertEquals(cal.showEvent("June"), "June 18 midterm exam\n");
+		
+		assertEquals(cal.showEvent("August"),"You don't have events in this month");
+		assertEquals(cal.showEvent("December"),"You don't have events in this month");
+		
+		
+		
 	}
+
 
 }
